db1014079734556e5af62070ebe730ef3763d930
dhirentheindian <dhirendhanani@gmail.com>
2017-07-04 00:48:14 +0800

    Basic BookService added

--- src/com/services/BookService.java
+++ src/com/services/BookService.java
@@ -28,6 +28,7 @@ public class BookService {
         Boolean out=false;
 
         
+        
         try {
             pstmt = connection.prepareStatement(sql);
             pstmt.setInt(1, b.getId());
