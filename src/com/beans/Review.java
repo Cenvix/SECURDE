4f89797df77b1e12d572218174597f903d0aaa8d
dhirentheindian <dhirendhanani@gmail.com>
2017-07-06 03:27:35 +0800

    Review Service added

---
+++ src/com/beans/Review.java
@@ -0,0 +1,59 @@
+package com.beans;
+
+public class Review {
+	public final static String COLUMN_ID = "reviewid";
+	public final static String COLUMN_USERID = "userid";
+	public final static String COLUMN_BOOKID = "bookid";
+	public final static String COLUMN_RATING = "rating";
+	public final static String COLUMN_REVIEW = "review";
+
+	public final static String TABLE_NAME = "reviews";
+	
+	private String review;
+	private int reviewID, userID,bookID,rating;
+	
+	
+	public Review(String review, int reviewID, int userID, int bookID, int rating) {
+		
+		this.review = review;
+		this.reviewID = reviewID;
+		this.userID = userID;
+		this.bookID = bookID;
+		this.rating = rating;
+	}
+	public Review() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getReview() {
+		return review;
+	}
+	public void setReview(String review) {
+		this.review = review;
+	}
+	public int getReviewID() {
+		return reviewID;
+	}
+	public void setReviewID(int reviewID) {
+		this.reviewID = reviewID;
+	}
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public int getBookID() {
+		return bookID;
+	}
+	public void setBookID(int bookID) {
+		this.bookID = bookID;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+	
+}
---
+++ src/com/services/ReviewService.java
@@ -0,0 +1,94 @@
+package com.services;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.DB.DBPool;
+import com.beans.Book;
+import com.beans.Review;
+
+public class ReviewService {
+	public static ArrayList<Review> getBookReviews(int bookid){
+		ArrayList<Review> reviews = new ArrayList();
+		
+		String sql = "Select * from "+ Review.TABLE_NAME+ " where "+Review.COLUMN_BOOKID+"=?;";
+		
+		Connection connection = DBPool.getInstance().getConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setInt(1, bookid);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()){
+				Review r = new Review();
+				r.setBookID(rs.getInt(Review.COLUMN_BOOKID));
+				r.setReviewID(rs.getInt(Review.COLUMN_ID));
+				r.setUserID(rs.getInt(Review.COLUMN_USERID));
+				r.setRating(rs.getInt(Review.COLUMN_RATING));
+				r.setReview(rs.getString(Review.COLUMN_REVIEW));
+				reviews.add(r);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			try {
+				rs.close();
+				pstmt.close();
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+				
+		return reviews;
+		
+	}
+	
+	public static boolean addReview(Review r){
+		boolean success = false;
+		
+		String sql = "insert into "+Review.TABLE_NAME+" values(?,?,?,?,?)";
+
+        Connection connection = DBPool.getInstance().getConnection();
+        PreparedStatement pstmt = null;
+        Boolean out=false;
+
+        
+        
+        try {
+            pstmt = connection.prepareStatement(sql);
+            
+            pstmt.setInt(1, r.getReviewID());
+            pstmt.setInt(2, r.getBookID());
+            pstmt.setInt(3, r.getUserID());
+            pstmt.setInt(4, r.getRating());
+            pstmt.setString(5, r.getReview());
+            pstmt.executeUpdate();
+            success=true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                pstmt.close();
+                connection.close();
+            } catch(SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return success;
+		
+		
+		return success;
+		
+	}
+}
